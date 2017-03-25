917e4387d1582262e421d95050a6c523acf87276
rajisuji <omkarini.enagandula31@gmail.com>
2017-03-25 20:17:15 +0530

    Added New Package and Classes

--- SeleniumPractise/src/com/selenium/java/HelloWorld.java
+++ SeleniumPractise/src/com/selenium/java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!Welcome to My World.");
+		System.out.println("Hello World!Welcome to My World.!");
 
 	}
 

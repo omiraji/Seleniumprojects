d3cd0f60a4f1b2f9a61d0ed449b60288f54fefe2
omiraji <omkarini.enagandula31@gmail.com>
2017-03-25 22:48:00 +0530

    Added New Addition Class as per given change request.

---
+++ SeleniumPractise/src/com/code/practice/Addition.java
@@ -0,0 +1,16 @@
+package com.code.practice;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		int c;
+		
+		c=(a+b);
+		System.out.println("Addition of Two Numbers is -"+c);
+
+	}
+
+}
